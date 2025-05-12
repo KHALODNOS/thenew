24d888cbcad3ccbeae7687937744c0a22764e446
Khalid Moumine <126973430+KHALODNOS@users.noreply.github.com>
2025-05-12 15:48:12 +0100

    Delete script-2.js

--- script-2.js
+++
@@ -1,7 +0,0 @@
-
-    document.querySelectorAll('.about .video-container .controlls .controll-btn').forEach(btn => {
-    btn.onclick = () => {
-        let src = btn.getAttribute('data-src');
-        document.querySelector('.about .video-container .video').src = src;
-    }
-})
